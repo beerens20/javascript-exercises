f420c156547c9159eafd003769b5ae0dfa4ec4e5
Mike Beerens <beerens20@gmail.com>
2022-07-12 14:56:46 -0400

    completed reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    if (string == " ") {
+        output = "";
+    } else {
+        let newString = "";
+        for (let i = string.length - 1; i >= 0; i--) {
+            newString += string[i];
+        }
+        output = newString;
+    };
+    return output;
 };
 
 // Do not edit below this line
