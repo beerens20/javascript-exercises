81b513ae1e9492ed8685160d80fa852f2586d28b
Mike Beerens <beerens20@gmail.com>
2022-07-12 15:31:29 -0400

    updated remove from array
    completed test 1 removing a single item from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    target = num - 1;
+    removed = array.splice(target, 1);
+    return array;
 };
 
 // Do not edit below this line
